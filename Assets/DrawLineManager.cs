8b7f35350fc27f0f1bc477bb17d0ef932c91b6bc
John <johneganireland@gmail.com>
2016-10-13 15:04:00 +0100

    For testing: Line color update

--- Assets/DrawLineManager.cs
+++ Assets/DrawLineManager.cs
@@ -9,13 +9,19 @@ private LineRenderer currLine;
 
 private int numClicks = 0;
 
+//set line color at the start and at the end
+private Color c1 = Color.red;
+private Color c2 = Color.red;
+
 void Update () {
 	SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
 	if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)){
-			GameObject go = new GameObject ();
-			currLine = go.AddComponent<LineRenderer> ();
+			GameObject go = new GameObject();
+			currLine = go.AddComponent<LineRenderer>();
+			currLine.material = new Material(Shader.Find("Particles/Additive"));
+			currLine.SetColors(c1, c2);
 
-			currLine.SetWidth(.1f , .1f);
+			currLine.SetWidth(.1f , .1f); // Adjust line width here
 
 			numClicks = 0;
 
