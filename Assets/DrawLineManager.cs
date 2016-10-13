e937088272afae7364180658fb19f461357c63a6
John <johneganireland@gmail.com>
2016-10-13 13:08:33 +0100

    drawing in 3D space
    Inserted script that allows controllers to draw in 3D space

---
+++ Assets/DrawLineManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawLineManager : MonoBehaviour {
+
+public SteamVR_TrackedObject trackedObj;
+
+private LineRenderer currLine;
+
+private int numClicks = 0;
+
+void Update () {
+	SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+	if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)){
+			GameObject go = new GameObject ();
+			currLine = go.AddComponent<LineRenderer> ();
+
+			currLine.SetWidth(.1f , .1f);
+
+			numClicks = 0;
+
+		} else if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			currLine.SetVertexCount(numClicks + 1);
+			currLine.SetPosition(numClicks, trackedObj.transform.position); //3d position of controller at any point in time
+			numClicks++;
+			}
+	}
+}
