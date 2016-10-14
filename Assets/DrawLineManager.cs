4ebb8fca1079e9b1e71bb449963832fb10a94a01
John <johneganireland@gmail.com>
2016-10-14 11:16:31 +0100

    [fix issue] line width adjustment
    Made mesh line width 10% of what it was in drawline manager script

--- Assets/DrawLineManager.cs
+++ Assets/DrawLineManager.cs
@@ -11,9 +11,6 @@ private MeshLineRenderer currLine;
 
 private int numClicks = 0;
 
-public int width; //Comment out this line and set width manually
-
-
 void Update () {
 	SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
 	if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)){
@@ -21,15 +18,13 @@ void Update () {
 			go.AddComponent<MeshFilter>();
 			go.AddComponent<MeshRenderer>();
 			currLine = go.AddComponent<MeshLineRenderer> ();
-
 			currLine.lmat = new Material(lMat);
-			currLine.setWidth(width); // Adjust line width here to 1f
-
+			currLine.setWidth(.1f); 
 			numClicks = 0;
-
 			} else if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
 			currLine.AddPoint(trackedObj.transform.position);
 			numClicks++;
+
 			} else if(device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
 			numClicks = 0;
 			currLine = null;
