833075a5c879d4d4f543607e65b8a1cd5708fd3e
John <johneganireland@gmail.com>
2016-10-17 10:36:46 +0100

    Updates for controller
    -Removed opinion on what device should be used for which in
    DrawLineManager.cs
    -Changed remove lines to application button

--- Assets/DrawLineManager.cs
+++ Assets/DrawLineManager.cs
@@ -5,9 +5,7 @@ public class DrawLineManager : MonoBehaviour {
 
 	public Material lMat;
 
-	public SteamVR_TrackedObject LeftTrackedObj; 
-
-	public SteamVR_TrackedObject RightTrackedObj;
+	public SteamVR_TrackedObject TrackedObj; 
 
 	private MeshLineRenderer currLine;
 
@@ -16,32 +14,29 @@ public class DrawLineManager : MonoBehaviour {
 	private GameObject meshes;
 
 	void Update () {
-		SteamVR_Controller.Device deviceL = SteamVR_Controller.Input((int)LeftTrackedObj.index);
-			if (deviceL.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)){
-				GameObject go = new GameObject(); 
-				go.AddComponent<MeshFilter>();
-				go.AddComponent<MeshRenderer>();
-				go.AddComponent<MeshCollider>();
-				currLine = go.AddComponent<MeshLineRenderer> ();
-				currLine.lmat = new Material(lMat);
-				currLine.setWidth(.1f);
-				currLine.tag = "MarkUp";
-			} else if (deviceL.GetTouch (SteamVR_Controller.ButtonMask.Trigger)){ 
-				currLine.AddPoint(LeftTrackedObj.transform.position);
-				numClicks++;
-				//need to add markup tag to created objects
-			} else if(deviceL.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)){ 
-				numClicks = 0;
-				currLine = null;
-			} 
-			if (currLine != null){ 
-				currLine.lmat.color = ColorManager.Instance.GetCurrentColor();
-			}
-
-		SteamVR_Controller.Device deviceR = SteamVR_Controller.Input((int)RightTrackedObj.index);
-		if (deviceR.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger)) {
+		SteamVR_Controller.Device device = SteamVR_Controller.Input((int)TrackedObj.index);
+		if (device.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			GameObject go = new GameObject (); 
+			go.AddComponent<MeshFilter> ();
+			go.AddComponent<MeshRenderer> ();
+			go.AddComponent<MeshCollider> ();
+			currLine = go.AddComponent<MeshLineRenderer> ();
+			currLine.lmat = new Material (lMat);
+			currLine.setWidth (.1f);
+			currLine.tag = "MarkUp";
+		} else if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) { 
+			currLine.AddPoint (TrackedObj.transform.position);
+			numClicks++;
+			//need to add markup tag to created objects
+		} else if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) { 
+			numClicks = 0;
+			currLine = null;
+		} else if (device.GetPressDown (SteamVR_Controller.ButtonMask.ApplicationMenu)) {
 			meshes = GameObject.FindWithTag ("MarkUp");
 			Destroy (meshes);
 		}
-	}
+			if (currLine != null){ 
+				currLine.lmat.color = ColorManager.Instance.GetCurrentColor();
+			}
+		}
 }
